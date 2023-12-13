3beadebe2599b4af0a9b662416b706a868f3eaeb
snippetsx <snippetsx@lcsa.ru>
2023-12-13 20:00:38 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lcsa_drawboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lcsa_drawboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lcsa_drawboard/glasscomponent.dart';
+void main(){
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : App()));
+}
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+          body: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image:AssetImage("assets/wallpaper.gif"),
+                    fit: BoxFit.fill
+                ),
+              ),
+              child: GlassComponent()
+          )
+      );
+  }
+}
