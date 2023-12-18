2cfaa11bec82d37466ed3d7935851b2d9234f8dc
snippetsx <snippetsx@lcsa.ru>
2023-12-18 23:44:19 +0300

    Added new file crypto.dart added crypto library for hashing the password on device

---
+++ lib/crypto.dart
@@ -0,0 +1,8 @@
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+String crypto256(String source) {
+  var bytes = utf8.encode(source);
+  var result = sha1.convert(bytes);
+  return result;
+}1
